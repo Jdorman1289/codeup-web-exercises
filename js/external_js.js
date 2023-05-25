7ced487bd2581112d26515a73d2cd27eaa9cc51d
Jesse Dorman <Jdorman1289@gmail.com>
2023-05-25 13:32:34 -0500

    add JS for how much you will receive in payment for this week

--- js/external_js.js
+++ js/external_js.js
@@ -9,6 +9,7 @@ let userColor = prompt("What is your favorite color?");
 alert(`Great, ${userColor} is my favorite color too!`);
 
 
+// how much do you owe the rental store?
 alert("You have rented some movies for your kids. The following questions will ask for how many days you have rented each movie.");
 let daysFilm1 = prompt("The little mermaid?");
 let daysFilm2 = prompt("Brother Bear?");
@@ -18,11 +19,24 @@ let pricePerDay = prompt("How much is the price for a movie per day?");
 daysFilm1 = parseFloat(daysFilm1);
 daysFilm2 = parseFloat(daysFilm2);
 daysFilm3 = parseFloat(daysFilm3);
-pricePerDay = parseFloat(pricePerDay)
+pricePerDay = parseFloat(pricePerDay);
 
 alert(`You owe the rental store ${(daysFilm1 + daysFilm2 + daysFilm3) * pricePerDay} dollars!`);
 
+// How much will you receive in payment for this week?
+let compPay1 = prompt("Now suppose you're working as a contractor for 3 companies and they pay you a different rate per hour. How much does the first company pay you an hour?");
+let compPay2 = prompt("How much does the second company pay you hourly?");
+let compPay3 = prompt("How much does the third company pay you hourly?");
 
-// Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+let compHours1 = prompt("Now, how many hours did you work for the first company this week?");
+let compHours2 = prompt("how many hours did you work for the second company this week?");
+let compHours3 = prompt("how many hours did you work for the third company this week?");
+
+compPay1 = parseFloat(compPay1) * parseFloat(compHours1);
+compPay2 = parseFloat(compPay2) * parseFloat(compHours2);
+compPay3 = parseFloat(compPay3) * parseFloat(compHours3);
+
+alert(`you made ${(compPay1 + compPay2 + compPay3)} dollar bucks!`)
+
 // A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
 // A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
