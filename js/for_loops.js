b8b61abe74d9c3c54173b8c28a02b776e1347dea
Jesse <Jdorman1289@gmail.com>
2023-06-05 11:58:49 -0500

    create files for loops exercise

---
+++ js/for_loops.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// console.log("Is this mic on?");
+
+
+function showMultiplicationTable() {
+
+}
+
+
+console.log(typeof showMultiplicationTable, "was returned, should return: function");
