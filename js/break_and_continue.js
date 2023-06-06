5038f3bb37b0245638af7639ce63ec44f4732dfd
Jesse <Jdorman1289@gmail.com>
2023-06-06 06:53:20 -0500

    finish loops exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,4 +1,4 @@
-let allTheOddWithinRange50 = [];
+const allTheOddWithinRange50 = [];
 for (let i = 1; i <= 50; i += 2) {
     allTheOddWithinRange50.push(i);
 }
@@ -9,4 +9,12 @@ do {
 } while (!allTheOddWithinRange50.includes(parseInt(userNumberToSkip)));
 
 console.log(`Number to skip is: ${userNumberToSkip}`);
+
+allTheOddWithinRange50.forEach(i => {
+    if (i !== parseInt(userNumberToSkip)) {
+        console.log(`Here is an odd number: ${i}`);
+    } else {
+        console.log(`Yikes! Skipping number: ${userNumberToSkip}`);
+    }
+})
 
---
+++ js/while.js
@@ -0,0 +1,23 @@
+let i = 1;
+let printOut = 2;
+
+while (i <= 16) {
+    console.log(printOut);
+    printOut += printOut;
+    i++;
+}
+
+let allCones = Math.floor(Math.random() * 50) + 50;
+
+do {
+    let conesBought = Math.floor(Math.random() * 4) + 1;
+    if (conesBought > allCones) {
+        console.log(`I can not sell you ${conesBought} I only have ${allCones} left!`);
+        continue;
+    } else if (conesBought < allCones) {
+        console.log(`${conesBought} cones sold!`);
+    } else {
+        console.log("Yay! I sold them all!");
+    }
+    allCones -= conesBought;
+} while (allCones > 0);
