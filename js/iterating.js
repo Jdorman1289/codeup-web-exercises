4b3cb471f1c41b484ec6ff8ac7c7a4ed90be0098
Jesse <Jdorman1289@gmail.com>
2023-06-06 09:19:53 -0500

    finish array introdruction exercise

--- js/iterating.js
+++ js/iterating.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     "use strict";
 
     /**
@@ -6,6 +6,7 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
+    let names = ['calvin', 'hobbes', 'zim', 'chuy'];
 
     /**
      * TODO:
@@ -13,22 +14,37 @@
      * array.
      */
 
+    console.log(names.length);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
 
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
 
+    for (let i = 0; i <= names.length - 1; i++) {
+        console.log(names[i]);
+    }
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+
+    names.forEach(name => {
+        console.log(name);
+    })
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
     /**
      * TODO:
@@ -43,5 +59,21 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+
+    function first(input) {
+        return input[0];
+    }
+
+    function second(input) {
+        return input[1];
+    }
+
+    function last(input) {
+        return input[4];
+    }
+
+    console.log(first([1, 2, 3, 4, 5])); // returns 1
+    console.log(second([1, 2, 3, 4, 5])); // returns 2
+    console.log(last([1, 2, 3, 4, 5])); // return 5
 
 })();
