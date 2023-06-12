edb9f915f496a652c24bd930e1705c48c12afcae
Jesse <Jdorman1289@gmail.com>
2023-06-12 09:13:12 -0500

    finish today's warmup

--- js/warmup.js
+++ js/warmup.js
@@ -1,15 +1,39 @@
-function makeUpperCase(input) {
-    if (typeof input !== "string") {
+// function makeUpperCase(input) {
+//     if (typeof input !== "string") {
+//         return false;
+//     } else {
+//         return input.toUpperCase();
+//     }
+// }
+//
+// console.log(`returned ${makeUpperCase('cat')} and should return CAT`);
+// console.log(`returned ${makeUpperCase('DoG')} and should return DOG`);
+// console.log(`returned ${makeUpperCase('CODEUP')} and should return CODEUP`);
+// console.log(`returned ${makeUpperCase(54)} and should return false`);
+// console.log(`returned ${makeUpperCase(true)} and should return false`);
+// console.log(`returned ${makeUpperCase(null)} and should return false`);
+// console.log(`returned ${makeUpperCase("5")} and should return 5`);
+
+// ================================= WARM UP
+
+// Write a function, sumIsEqual, that takes in three inputs. The function should return
+// true if the sum of the first two inputs are equal to the third input. If any of the
+// inputs are not numbers, return false.
+
+function sumIsEqual(in1, in2, in3) {
+    if (typeof in1 !== "number" || typeof in2 !== "number" || typeof in3 !== "number") {
         return false;
     } else {
-        return input.toUpperCase();
+        let sumOfIn1AndIn2 = in1 + in2;
+        return (sumOfIn1AndIn2 === in3);
     }
 }
 
-console.log(`returned ${makeUpperCase('cat')} and should return CAT`);
-console.log(`returned ${makeUpperCase('DoG')} and should return DOG`);
-console.log(`returned ${makeUpperCase('CODEUP')} and should return CODEUP`);
-console.log(`returned ${makeUpperCase(54)} and should return false`);
-console.log(`returned ${makeUpperCase(true)} and should return false`);
-console.log(`returned ${makeUpperCase(null)} and should return false`);
-console.log(`returned ${makeUpperCase("5")} and should return 5`);
+
+console.log(sumIsEqual(1, 2, 3)); // returns true
+console.log(sumIsEqual(2, 2, 4)); // returns true
+console.log(sumIsEqual(10, 2, 12)); // returns true
+console.log(sumIsEqual(10, false, 12)); // returns false
+console.log(sumIsEqual('10', '2', 12)); // returns false
+console.log(sumIsEqual('fred', 'bob', 'hello')); // returns false
+
