aac62f8afadb8a2906e7fd522030af1d9a1ce3d5
Jesse <Jdorman1289@gmail.com>
2023-07-13 14:59:38 -0500

    add geocoder to mapbox

--- js/mapbox.js
+++ js/mapbox.js
@@ -1,4 +1,6 @@
-const map = initializeMap()
+const map = initializeMap();
+const marker = createMarker();
+const popup = createPopup();
 
 function initializeMap() {
     mapboxgl.accessToken = MAPBOX_TOKEN;
@@ -11,9 +13,14 @@ function initializeMap() {
     }
     return new mapboxgl.Map(mapOptions);
 }
+function createMarker() {
+    return new mapboxgl.Marker().setLngLat([-98.4916, 29.4252]).addTo(map);
+}
+function createPopup() {
+    return new mapboxgl.Popup().setLngLat([-98.4916, 29.4252]).setHTML(`
+    <h1>Sup</h1>
+    <p>My man!</p>
+    `)
+}
 
-
-map.setZoom(1);
-
-
-
+marker.setPopup(popup);
