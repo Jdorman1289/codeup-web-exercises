464a5a8fdbc5b3e66525b768450c33678a1a5e70
Jesse <Jdorman1289@gmail.com>
2023-07-13 10:34:42 -0500

    finish basic structre for ajax exercise

--- js/ajax-lesson.js
+++ js/ajax-lesson.js
@@ -6,7 +6,29 @@ function renderTableHead(data) {
     return info.join('');
 }
 
+function renderItemQuantity(data) {
+    let info = [];
+    data.forEach(item => {
+        info.push(`<td>Quantity: ${item.quantity}</td>`);
+    })
+    return info.join('');
+}
 
+function renderItemPrice(data) {
+    let info = [];
+    data.forEach(item => {
+        info.push(`<td>Price: $${item.price}</td>`);
+    })
+    return info.join('');
+}
+
+function renderItemCategories(data) {
+    let info = [];
+    data.forEach(item => {
+        info.push(`<td>Type: ${item.categories}</td>`);
+    })
+    return info.join('');
+}
 
 $.ajax({
     url: 'data/inventory.json',
@@ -14,5 +36,7 @@ $.ajax({
 }).done((data) => {
     console.log(data)
     document.querySelector('tr').innerHTML = `${renderTableHead(data)}`;
-
+    document.querySelector('tbody').firstElementChild.innerHTML = `${renderItemQuantity(data)}`;
+    document.querySelector('tbody').firstElementChild.nextElementSibling.innerHTML = `${renderItemPrice(data)}`;
+    document.querySelector('tbody').lastElementChild.innerHTML = `${renderItemCategories(data)}`;
 })
