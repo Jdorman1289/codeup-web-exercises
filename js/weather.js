5074b1136408817711eca2c148fe67a71ec11243
Jesse <Jdorman1289@gmail.com>
2023-07-16 08:38:49 -0500

    update weather icons based on weather for location

--- js/weather.js
+++ js/weather.js
@@ -8,40 +8,141 @@ function getWeatherURL(lat, lon) {
     return `${OPEN_WEATHER_URL}?lat=${lat}&lon=${lon}&units=imperial&appid=${OPEN_WEATHER_APPID}`;
 }
 
-const ALAMO_COORDINATES = ['29.4260', '-98.4861'];
-
-// TODO: log URL
-
-// console.log(getWeatherURL(ALAMO_COORDINATES[0], ALAMO_COORDINATES[1]));
-
-const URL = getWeatherURL(...ALAMO_COORDINATES);
-
-// TODO: log full response from API
-
-$.ajax(URL).done(data => {
-    console.log(data);
-}).fail(console.error);
+const WEATHER_COORDINATES = ['29.4260', '-98.4861'];
 
 
-// TODO: log the humidity for all 5 days
+const URL = getWeatherURL(...WEATHER_COORDINATES);
 
-// $.ajax(getWeatherURL(...ALAMO_COORDINATES))
-//     .done((data) => {
-//
-//         data.list.forEach((day, index) => {
-//             if (index % 8 === 0) {
-//                 console.log(day.main.humidity);
-//             }
-//         });
-//
-//         // OR
-//
-//         for (let i = 0; i < data.list.length; i += 8) {
-//             console.log(data.list[i].main.humidity);
-//         }
-//
-//     })
-//     .fail(console.error);
+const Day1 = document.querySelector('.day1');
+const Day2 = document.querySelector('.day2');
+const Day3 = document.querySelector('.day3');
+const Day4 = document.querySelector('.day4');
+const Day5 = document.querySelector('.day5');
+
+
+// $.ajax(URL).done(data => {
+//     console.log(data);
+// }).fail(console.error);
+
+
+$.ajax(URL)
+    .done((data) => {
+        let weatherArray = [];
+
+        data.list.forEach((day, index) => {
+            if (index % 8 === 0) {
+                weatherArray.push(day.weather[0].description);
+            }
+        });
+        showWeather(weatherArray);
+    })
+    .fail(console.error);
+
+function showWeather(weatherArray) {
+    Day1.lastElementChild.lastElementChild.innerText = weatherArray[0];
+    if (weatherArray[0].includes('cloud')) {
+        Day1.firstElementChild.classList.add('card-storm');
+        Day1.firstElementChild.firstElementChild.removeAttribute('class');
+    } else if (weatherArray[0].includes('rain')) {
+        Day1.firstElementChild.classList.add('card-rain');
+        Day1.firstElementChild.firstElementChild.classList.add('rain');
+    } else if (weatherArray[0].includes('clear sky')) {
+        Day1.firstElementChild.classList.add('card-sunny');
+        Day1.firstElementChild.firstElementChild.classList.add('sunny');
+    } else if (weatherArray[0].includes('snow')) {
+        Day1.firstElementChild.classList.add('card-snow');
+        Day1.firstElementChild.firstElementChild.classList.add('snow');
+    } else if (weatherArray[0].includes('storm')) {
+        Day1.firstElementChild.classList.add('card-storm');
+        Day1.firstElementChild.firstElementChild.classList.add('storm');
+    } else {
+        Day1.firstElementChild.classList.add('card-snow');
+        Day1.firstElementChild.firstElementChild.removeAttribute('class');
+    }
+
+    Day2.lastElementChild.lastElementChild.innerText = weatherArray[1];
+    if (weatherArray[1].includes('cloud')) {
+        Day2.firstElementChild.classList.add('card-storm');
+        Day1.firstElementChild.firstElementChild.removeAttribute('class');
+    } else if (weatherArray[1].includes('rain')) {
+        Day2.firstElementChild.classList.add('card-rain');
+        Day2.firstElementChild.firstElementChild.classList.add('rain');
+    } else if (weatherArray[1].includes('clear sky')) {
+        Day2.firstElementChild.classList.add('card-sunny');
+        Day2.firstElementChild.firstElementChild.classList.add('sunny');
+    } else if (weatherArray[1].includes('snow')) {
+        Day2.firstElementChild.classList.add('card-snow');
+        Day2.firstElementChild.firstElementChild.classList.add('snow');
+    } else if (weatherArray[1].includes('storm')) {
+        Day2.firstElementChild.classList.add('card-storm');
+        Day2.firstElementChild.firstElementChild.classList.add('storm');
+    } else {
+        Day2.firstElementChild.classList.add('card-snow');
+        Day2.firstElementChild.firstElementChild.removeAttribute('class');
+    }
+
+    Day3.lastElementChild.lastElementChild.innerText = weatherArray[2];
+    if (weatherArray[2].includes('cloud')) {
+        Day3.firstElementChild.classList.add('card-storm');
+        Day3.firstElementChild.firstElementChild.removeAttribute('class');
+    } else if (weatherArray[2].includes('rain')) {
+        Day3.firstElementChild.classList.add('card-rain');
+        Day3.firstElementChild.firstElementChild.classList.add('rain');
+    } else if (weatherArray[2].includes('clear sky')) {
+        Day3.firstElementChild.classList.add('card-sunny');
+        Day3.firstElementChild.firstElementChild.classList.add('sunny');
+    } else if (weatherArray[2].includes('snow')) {
+        Day3.firstElementChild.classList.add('card-snow');
+        Day3.firstElementChild.firstElementChild.classList.add('snow');
+    } else if (weatherArray[2].includes('storm')) {
+        Day3.firstElementChild.classList.add('card-storm');
+        Day3.firstElementChild.firstElementChild.classList.add('storm');
+    } else {
+        Day3.firstElementChild.classList.add('card-snow');
+        Day3.firstElementChild.firstElementChild.removeAttribute('class');
+    }
+
+    Day4.lastElementChild.lastElementChild.innerText = weatherArray[3];
+    if (weatherArray[3].includes('cloud')) {
+        Day4.firstElementChild.classList.add('card-storm');
+        Day4.firstElementChild.firstElementChild.removeAttribute('class');
+    } else if (weatherArray[3].includes('rain')) {
+        Day4.firstElementChild.classList.add('card-rain');
+        Day4.firstElementChild.firstElementChild.classList.add('rain');
+    } else if (weatherArray[3].includes('clear sky')) {
+        Day4.firstElementChild.classList.add('card-sunny');
+        Day4.firstElementChild.firstElementChild.classList.add('sunny');
+    } else if (weatherArray[3].includes('snow')) {
+        Day4.firstElementChild.classList.add('card-snow');
+        Day4.firstElementChild.firstElementChild.classList.add('snow');
+    } else if (weatherArray[3].includes('storm')) {
+        Day4.firstElementChild.classList.add('card-storm');
+        Day4.firstElementChild.firstElementChild.classList.add('storm');
+    } else {
+        Day4.firstElementChild.classList.add('card-snow');
+        Day4.firstElementChild.firstElementChild.removeAttribute('class');
+    }
+    Day5.lastElementChild.lastElementChild.innerText = weatherArray[4];
+    if (weatherArray[4].includes('cloud')) {
+        Day5.firstElementChild.classList.add('card-storm');
+        Day5.firstElementChild.firstElementChild.removeAttribute('class');
+    } else if (weatherArray[4].includes('rain')) {
+        Day5.firstElementChild.classList.add('card-rain');
+        Day5.firstElementChild.firstElementChild.classList.add('rain');
+    } else if (weatherArray[4].includes('clear sky')) {
+        Day5.firstElementChild.classList.add('card-sunny');
+        Day5.firstElementChild.firstElementChild.classList.add('sunny');
+    } else if (weatherArray[4].includes('snow')) {
+        Day5.firstElementChild.classList.add('card-snow');
+        Day5.firstElementChild.firstElementChild.classList.add('snow');
+    } else if (weatherArray[4].includes('storm')) {
+        Day5.firstElementChild.classList.add('card-storm');
+        Day5.firstElementChild.firstElementChild.classList.add('storm');
+    } else {
+        Day5.firstElementChild.classList.add('card-snow');
+        Day5.firstElementChild.firstElementChild.removeAttribute('class');
+    }
+}
 
 
 // TODO: log the min and max temp for each day
