846c5e61b6c7b3048510f45fedb6309b19ec3ea6
Jesse <Jdorman1289@gmail.com>
2023-07-20 12:56:32 -0500

    partial JS2 lecture notes

---
+++ js/mfr-lesson.js
@@ -0,0 +1,28 @@
+(() => {
+    let url = 'data/houses.json';
+    let apiOptions = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    fetch(url, apiOptions).then((response) => {
+        return response.json()
+    }).then(data => {
+        //filter
+        let threeOrMoreBathHousesAtThisZip = data.filter(house => parseFloat(house.baths) >= 3).filter(house => house.addressZipcode === "78748");
+        // console.log(threeOrMoreBathHousesAtThisZip);
+
+        //map
+        let housesPriceRange = data.map(house => `Address:\n${house.address}\n\nCan I afford it?\n${(house.price > 600_000) ? `No` : `Yes`}`);
+        for(let house of housesPriceRange) {
+            console.log(house);
+        }
+
+
+
+
+    })
+
+})();
