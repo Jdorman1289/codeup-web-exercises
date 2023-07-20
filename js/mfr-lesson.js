d11e51085fa51b3a67c17b8093f5d51d8c30fb1a
Jesse <Jdorman1289@gmail.com>
2023-07-20 14:02:12 -0500

    finish basic lecture notes for map filter and reduce

--- js/mfr-lesson.js
+++ js/mfr-lesson.js
@@ -10,18 +10,22 @@
     fetch(url, apiOptions).then((response) => {
         return response.json()
     }).then(data => {
+
         //filter
         let threeOrMoreBathHousesAtThisZip = data.filter(house => parseFloat(house.baths) >= 3).filter(house => house.addressZipcode === "78748");
         // console.log(threeOrMoreBathHousesAtThisZip);
 
         //map
         let housesPriceRange = data.map(house => `Address:\n${house.address}\n\nCan I afford it?\n${(house.price > 600_000) ? `No` : `Yes`}`);
-        for(let house of housesPriceRange) {
-            console.log(house);
-        }
 
+        // for (let house of housesPriceRange) {
+        //     console.log(house);
+        // }
 
+        //reduce
+        let averageHousePrice = data.reduce((avgPrice, house) => parseFloat(house.price) + avgPrice, 0) / data.length;
 
+        console.log(averageHousePrice.toLocaleString("en-US", {style: "currency", currency: "USD"}));
 
     })
 
