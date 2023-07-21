3c27cc5cfa5889b6ffb13f82379af59b76fa9301
Jesse <Jdorman1289@gmail.com>
2023-07-21 10:26:48 -0500

    refactor promise exercise so that code is in an IFFE

--- js/promises.js
+++ js/promises.js
@@ -1,20 +1,24 @@
-let userName = prompt('which GitHub username?')
+(() => {
 
-let url = `https://api.github.com/users/${userName}/events/public`;
-let apiOptions = {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-        'authorization': `token ${GITHUB_MONTH_KEY}`
+    let userName = prompt('which GitHub username?')
+
+    let url = `https://api.github.com/users/${userName}/events/public`;
+    let apiOptions = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'authorization': `token ${GITHUB_MONTH_KEY}`
+        }
     }
-}
 
-fetch(url, apiOptions).then((response) => {
-    return response.json()
-}).then(data => {
-    // console.log(data);
+    fetch(url, apiOptions).then((response) => {
+        return response.json()
+    }).then(data => {
+        // console.log(data);
 
-    document.querySelector('#user-image').innerHTML = `<img = src="${data[0].actor.avatar_url}" alt="user profile image">`
-    document.querySelector('p').innerHTML = `Their last push was to ${data[0].repo.name} at ${data[0].created_at}`
+        document.querySelector('#user-image').innerHTML = `<img = src="${data[0].actor.avatar_url}" alt="user profile image">`
+        document.querySelector('p').innerHTML = `Their last push was to ${data[0].repo.name} at ${data[0].created_at}`
 
-})
+    })
+
+})()
