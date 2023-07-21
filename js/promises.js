133425d6d440105f09b5360ac4e157db8db0e68b
Jesse <Jdorman1289@gmail.com>
2023-07-21 12:31:27 -0500

    refactor github api with some style

--- js/promises.js
+++ js/promises.js
@@ -1,7 +1,4 @@
-(() => {
-
-    let userName = prompt('which GitHub username?')
-
+const getUser = userName => {
     let url = `https://api.github.com/users/${userName}/events/public`;
     let apiOptions = {
         method: 'GET',
@@ -11,14 +8,28 @@
         }
     }
 
-    fetch(url, apiOptions).then((response) => {
+    return fetch(url, apiOptions).then((response) => {
         return response.json()
-    }).then(data => {
-        // console.log(data);
-
-        document.querySelector('#user-image').innerHTML = `<img = src="${data[0].actor.avatar_url}" alt="user profile image">`
-        document.querySelector('p').innerHTML = `Their last push was to ${data[0].repo.name} at ${data[0].created_at}`
-
+    }).catch((error) => {
+        console.log(error.message);
     })
+}
+
+
+(() => {
+
+    document.querySelector('button').addEventListener('click', () => {
+            let userName = document.querySelector('input').value;
+            getUser(userName).then(user => {
+
+                document.querySelector('#user-image').innerHTML = `<img class="rounded-pill" width="200px" src="${user[0].actor.avatar_url}" alt="user profile image">`
+                document.querySelector('span').innerHTML = `Their last push was to this repo: ${user[0].repo.name} at this time: ${user[0].created_at}`
+
+
+            }).catch((error) => {
+                console.log(error.message);
+            })
+        }
+    )
 
 })()
